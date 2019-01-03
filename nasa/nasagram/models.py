2c4813b3b2545c5a86d5f7d1d8f018986cab4fd0
J <jeremy.j.holloway@gmail.com>
2019-01-03 16:09:43 -0500

    second version

---
+++ nasa/nasagram/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.urls import reverse_lazy
+
+# Create your models here.
+class NasaComment(models.Model):
+    rating = models.IntegerField()
+    comments = models.TextField()
+    favorite = models.BooleanField()
+    image_url = models.URLField()
+    date = models.DateField()
+
+
+    def __str__(self):
+        return f'{self.comments}, {self.image_url}, {self.date}, {self.rating}, {self.favorite}'
+
+    def get_absolute_url(self):
+        return reverse_lazy('nasa_comment_detail', args = [str(self.id)])
